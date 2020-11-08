af41e1094a8c5aef588c334f4e212ea062eeb542
Nuno Santos <61665536+4E756E6F@users.noreply.github.com>
2020-11-08 16:28:41 +0000

    Setting up the board

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import pygame
 import sys
+import random
+from pygame import Color
+from pygame.math import Vector2
+
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x, self.pos.y, cell_size, cell_size)
+        pygame.draw.rect(screen, (255, 25, 25), fruit_rect)
+
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 25
+screen = pygame.display.set_mode(
+    (cell_size * cell_number, cell_size * cell_number))
+clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.quit:
             pygame.quit()
             sys.exit()
-    # **DRAW ALL OUR ELEMENTS
+
+    screen.fill((175, 215, 70))
+    fruit.draw_fruit()
     pygame.display.update()
+    clock.tick(60)
