2adbf20f1a0467b347e5dd6a8b32b43c96a6e5b9
Nuno Santos <61665536+4E756E6F@users.noreply.github.com>
2020-11-09 00:37:48 +0000

    added the snake body and a new background pattern

--- main.py
+++ main.py
@@ -10,13 +10,66 @@ class SNAKE:
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
+        # ??    ▼▼  DIFERENTE HEAD POSITION  ▼▼
+        self.head_up = pygame.image.load(
+            'Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load(
+            'Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load(
+            'Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load(
+            'Graphics/head_left.png').convert_alpha()
+        # ??    ▼▼  DIFERENTE TAIL POSITION  ▼▼
+        self.tail_up = pygame.image.load(
+            'Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load(
+            'Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load(
+            'Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load(
+            'Graphics/tail_left.png').convert_alpha()
+        # ??    ▼▼  DIFERENTE BODY POSITION  ▼▼
+        self.body_vertical = pygame.image.load(
+            'Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load(
+            'Graphics/body_horizontal.png').convert_alpha()
+        # ??    ▼▼  DIFERENTE BODY TURNS  ▼▼
+        self.body_tr = pygame.image.load(
+            'Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load(
+            'Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load(
+            'Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load(
+            'Graphics/body_bl.png').convert_alpha()
 
     def draw_snake(self):
-        for block in self.body:
+        self.update_head_graphics()
+        self.update_tail_graphics()
+        for index, block in enumerate(self.body):
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (51, 255, 51), block_rect)
+            if index == 0:
+                screen.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br, block_rect)
 
     def move_snake(self):
         if self.new_block == True:
@@ -31,6 +84,30 @@ class SNAKE:
 
     def add_block(self):
         self.new_block = True
+
+    # ** This verifies the position of the head and changes the orientation of the head to the correct one
+    def update_head_graphics(self):
+        head_body_relation = self.body[1] - self.body[0]
+        if head_body_relation == Vector2(1, 0):
+            self.head = self.head_left
+        elif head_body_relation == Vector2(-1, 0):
+            self.head = self.head_right
+        elif head_body_relation == Vector2(0, 1):
+            self.head = self.head_up
+        elif head_body_relation == Vector2(0, -1):
+            self.head = self.head_down
+
+    # ** This verifies the position of the tail and changes the orientation of the tail to the correct one
+    def update_tail_graphics(self):
+        tail_body_relation = self.body[-2] - self.body[-1]
+        if tail_body_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_body_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_body_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_body_relation == Vector2(0, -1):
+            self.tail = self.tail_down
 
 
 class FRUIT:
@@ -61,6 +138,7 @@ class MAIN:
         self.check_colision()
 
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
 
@@ -76,6 +154,22 @@ class MAIN:
             if block == self.snake.body[0]:
                 self.game_over()
 
+    def draw_grass(self):
+        grass_color = (96, 96, 96)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(
+                            col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(
+                            col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+
     def game_over(self):
         pygame.quit()
         sys.exit()
@@ -88,7 +182,7 @@ screen = pygame.display.set_mode(
     (cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
 ramen = pygame.image.load('Graphics/ramen.png').convert_alpha()
-ramen = pygame.transform.scale(ramen, (52, 52))
+ramen = pygame.transform.scale(ramen, (35, 35))
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
@@ -116,7 +210,7 @@ while True:
                 if main_game.snake.direction.x != 1:
                     main_game.snake.direction = Vector2(-1, 0)
 
-    screen.fill((0, 0, 0))
+    screen.fill((64, 64, 64))
     main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)
